705dca17877505399cbfad020f864a0306fc007a
Rommy Christensen <rommycy00@gmail.com>
2022-01-11 15:54:19 +0700

    modif signup test

--- ta-auth/src/routes/__test__/signup.test.ts
+++ ta-auth/src/routes/__test__/signup.test.ts
@@ -1,16 +1,17 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-// it('returns a 201 on successfull signup', async () => {
-//     return request(app)
-//         .post('/api/auth/signup')
-//         .send({
-//             "username": "dominator",
-//             "email": "dominatorranger@gmail.com",
-//             "firstname": "Dominator",
-//             "lastname": "Ranger",
-//             "password": "123456789",
-//             "password_confirmation" : "123456789"
-//         })
-//         .expect(201);
-// });
+it('returns a 201 on successfull signup', async () => {
+    return request(app)
+        .post('/api/auth/signup')
+        .send({
+            "username": "dominator",
+            "email": "dominatorranger@gmail.com",
+            "firstname": "Dominator",
+            "lastname": "Ranger",
+            "password": "123456789",
+            "password_confirmation" : "123456789",
+            "role": "worker"
+        })
+        .expect(201);
+});
