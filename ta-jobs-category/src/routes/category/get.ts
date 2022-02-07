efae4b51cf36334e68d2e146fda62af9d4c3c3d0
Rommy Christensen <rommycy00@gmail.com>
2022-02-07 12:39:00 +0700

    add new endpoint for get category

--- ta-jobs-category/src/routes/category/get.ts
+++ ta-jobs-category/src/routes/category/get.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import categoryDoc from '../../models/category';
 import { checkHeaderAdmin } from '../../middlewares/validate-header-admin';
 import { Error } from 'mongoose';
+import { validateHeader } from '@ta-vrilance/common';
 
 const router = express.Router();
 
@@ -11,7 +12,19 @@ async (req: Request, res: Response) => {
     const cats = await categoryDoc.getAll()
     
     if(cats){
-        return res.status(201).send(cats);
+        return res.status(200).send(cats);
+    }else{
+        throw new Error('Get Data Category Failed');
+    }
+});
+
+router.get('/api/jobscat/category',
+validateHeader,
+async (req: Request, res: Response) => {
+    const cats = await categoryDoc.getAll()
+    
+    if(cats){
+        return res.status(200).send(cats);
     }else{
         throw new Error('Get Data Category Failed');
     }
