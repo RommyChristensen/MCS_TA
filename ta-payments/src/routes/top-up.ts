bd46d60786550215077e817eb2be8d7b4952def1
Rommy Christensen <rommycy00@gmail.com>
2022-03-29 17:42:29 +0700

    midtrans

--- ta-payments/src/routes/top-up.ts
+++ ta-payments/src/routes/top-up.ts
@@ -1,6 +1,7 @@
 import { body } from "express-validator";
 import express, { Request, Response } from "express";
 import { validateHeader } from "@ta-vrilance/common";
+import { coreApiClient } from "../helpers/v-midtrans-client";
 
 const router = express.Router();
 
@@ -11,7 +12,23 @@ interface JwtPayload {
 router.post('/api/payments/topup', 
 validateHeader,
 async (req: Request, res: Response) => {
+    let parameter = {
+        "payment_type": "bank_transfer",
+        "bank_transfer": {
+            "bank": "permata"
+        },
+        "transaction_details": {
+            "order_id": "C17550",
+            "gross_amount": 145000
+        },
+        "custom_field1": "custom field 1 content",
+        "custom_field2": "custom field 2 content",
+        "custom_field3": "custom field 3 content"
+    };
 
+    coreApiClient.charge(parameter).then((chargeResponse: string) => {
+        return res.send(chargeResponse);
+    });
 });
 
 export { router as topUpRouter }
