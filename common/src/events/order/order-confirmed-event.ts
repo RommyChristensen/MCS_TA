6c4b0cfb96ce4792d219f838e097a9f1c18654f8
Rommy Christensen <rommycy00@gmail.com>
2022-05-07 13:23:30 +0700

    Updates

--- common/src/events/order/order-confirmed-event.ts
+++ common/src/events/order/order-confirmed-event.ts
@@ -5,6 +5,10 @@ export interface OrderConfirmedEvent {
     subject: Subjects.OrderConfirmed; 
     data: {
         id: string;
+        order_id: string;
+        worker_id: string,
+        orderer_id: string,
+        total_payment: string,
         _v: number;
     }; 
 }
