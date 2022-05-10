c5affcf6197dace34785daa7d698cdfde41049a1
Rommy Christensen <rommycy00@gmail.com>
2022-05-10 16:40:46 +0700

    test

--- ta-payments/src/routes/get.ts
+++ ta-payments/src/routes/get.ts
@@ -1,7 +1,8 @@
 import { body } from "express-validator";
 import express, { Request, Response } from "express";
-import { validateHeader } from "@ta-vrilance/common";
+import { BadRequestError, validateHeader } from "@ta-vrilance/common";
 import userDoc from "../models/user";
+import { OrderPaymentStatus } from "../models/order-payment-status";
 
 const router = express.Router();
 
@@ -10,6 +11,24 @@ validateHeader,
 async (req: Request, res: Response) => {
     const user = req.params.user_id ? await userDoc.findById(req.params.user_id) : await userDoc.getAll();
     return res.send(user);
+});
+
+router.get('/api/payments/orderhistory/:userId', validateHeader, async (req: Request, res: Response) => {
+    if(!req.params.userId) throw new BadRequestError('User ID Tidak Valid');
+
+    const userId = req.params.userId;
+    const user = await userDoc.findById(userId);
+
+    if(user){
+        const paidOrders = user.order_history.filter(o => o.status == OrderPaymentStatus.done);
+        const unpaidOrders = user.order_history.filter(o => o.status == OrderPaymentStatus.pending);
+        return res.send({
+            paidOrders,
+            unpaidOrders
+        });
+    }else{
+        return res.status(404).send({ message: "User Tidak Ditemukan" });
+    }
 });
 
 export { router as getRouter }
