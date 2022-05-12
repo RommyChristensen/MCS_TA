e067fad9a9f76f4f093c261cfdfe4e20bf5e2054
Rommy Christensen <rommycy00@gmail.com>
2022-05-12 17:56:13 +0700

    ads worker service

---
+++ ta-ads-worker/src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@ta-vrilance/common';
+
+// IMPORT ROUTERS
+
+
+// END IMPORT ROUTERS
+
+const app = express();
+
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: false
+    })
+);
+
+// USE ROUTERS
+
+// END USE ROUTERS
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
