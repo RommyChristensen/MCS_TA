c0c58cc75410262c0bcfb4d733a714985c6b1793
Rommy Christensen <rommycy00@gmail.com>
2022-05-14 10:37:05 +0700

    notif

--- ta-orders/src/routes/accept.ts
+++ ta-orders/src/routes/accept.ts
@@ -7,6 +7,7 @@ import userDoc from "../models/user";
 import jwt from 'jsonwebtoken';
 import jobDoc from "../models/job";
 import { OrderAcceptedPublisher } from "../events/publishers/order-accepted-publisher";
+import { MessageNotificationPublisher } from "../events/publishers/notification-publisher";
 
 const router = express.Router();
 
@@ -51,6 +52,20 @@ async (req: Request, res: Response) => {
         order_date: order.order_date as Date,
         _v: updatedOrder._v
     });
+
+    if(updatedOrder.order_type == 1){
+        new MessageNotificationPublisher(natsWrapper.client).publish({
+            user_id: order.orderer_id as string,
+            topic: "Pesanan Diterima",
+            message: "Pesanan dengan id " + order_id + " dengan judul " + job.job_title + " telah diterima oleh pencari jasa. Anda dapat melakukan pekerjaan ini hingga tanggal pekerjaan dilakukan."
+        });
+    }else{
+        new MessageNotificationPublisher(natsWrapper.client).publish({
+            user_id: job.job_created_by as string,
+            topic: "Pesanan Diterima",
+            message: "Pesanan dengan id " + order_id + " dengan judul " + job.job_title + " telah diterima oleh pencari kerja. Anda dapat menunggu pencari kerja melakukan pekerjaan hingga tanggal pekerjaan dilakukan."
+        });
+    }
 
     return res.status(200).send(updatedOrder);
 });
