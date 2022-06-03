591cf9c4293864a86d86d5eb7dbcf1ef2d6cc467
Rommy Christensen <rommycy00@gmail.com>
2022-06-03 13:31:11 +0700

    bar chart

--- ta-client/src/pages/report/Report.js
+++ ta-client/src/pages/report/Report.js
@@ -1,4 +1,5 @@
 import * as mui from '@mui/material';
+import { Bar } from 'react-chartjs-2';
 
 const Report = () => {
 
@@ -10,6 +11,27 @@ const Report = () => {
                 }}>
             <mui.Grid>
                 <mui.Typography fontWeight={500} variant="h5">Reports</mui.Typography>
+                <Bar 
+                    width={400}
+                    height={300}
+                    data={
+                        {
+                            labels,
+                            datasets: [
+                                {
+                                label: 'Dataset 1',
+                                data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+                                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                                },
+                                {
+                                label: 'Dataset 2',
+                                data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+                                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                                },
+                            ],
+                        }
+                    }
+                />
             </mui.Grid>
         </mui.Container>
     )
