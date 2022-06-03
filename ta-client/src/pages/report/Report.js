979a26280c5cb5ef2bb9233494ce481f38120a76
Rommy Christensen <rommycy00@gmail.com>
2022-06-03 13:53:16 +0700

    bar chart

--- ta-client/src/pages/report/Report.js
+++ ta-client/src/pages/report/Report.js
@@ -1,5 +1,14 @@
 import * as mui from '@mui/material';
-import { Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+  import { Bar } from 'react-chartjs-2';
 
 const Report = () => {
     const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
