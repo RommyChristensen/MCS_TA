b38caebb0aa7320c915f94381659c547f7748c01
Rommy Christensen <rommycy00@gmail.com>
2022-06-21 14:01:08 +0700

    order

--- ta-orders/src/routes/get.ts
+++ ta-orders/src/routes/get.ts
@@ -3,7 +3,6 @@ import userDoc from '../models/user';
 import orderDoc from '../models/order';
 import jobDoc, { Job } from '../models/job';
 import { BadRequestError, OrderStatus, validateHeader } from '@ta-vrilance/common';
-import { listenerCount } from 'nodemailer/lib/xoauth2';
 
 const router = express.Router();
 
@@ -101,15 +100,20 @@ async (req: Request, res: Response) => {
 
     let order = await orderDoc.findById(orderId);
 
-    const job = await jobDoc.findById(order.job_id as string);
-
-    order["job_id"] = job;
-
     if(!order){
         throw new BadRequestError("Data Order Tidak Ditemukan");
     }
 
-    return res.send(order);
+    const job = await jobDoc.findById(order.job_id as string);
+    order["job_id"] = job;
+
+    let orders = await orderDoc.getByJobId(job.id);
+
+    const r  = orders.map(o => {
+        o["job_id"] = job;
+        return o;
+    });
+    return res.send(r);
 });
 
 router.get('/api/orders/hirer/:hirerId', validateHeader, async (req: Request, res: Response) => {
